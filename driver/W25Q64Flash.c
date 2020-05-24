e4deda2e7bfb842aad44b772db4caeda764f0d59
crawel-john <895961454@qq.com>
2020-05-24 23:07:55 +0800

    Signed-off-by: crawel-john <895961454@qq.com>

--- driver/W25Q64Flash.c
+++ driver/W25Q64Flash.c
@@ -22,20 +22,6 @@
 /*  Definitions                                                              */
 /*                                                                           */
 /*****************************************************************************/
-#define W25_RCC1                    RCU_GPIOA
-#define W25_RCC2                    RCU_GPIOB
-#define CLK_GPIO                   GPIOB
-#define CLK_PIN                    (GPIO_PIN_3)
-
-#define DO_GPIO                   GPIOB
-#define DO_PIN                    (GPIO_PIN_4)
-
-#define DI_GPIO                   GPIOB
-#define DI_PIN                    (GPIO_PIN_5)
-
-#define CS_GPIO                   GPIOA
-#define CS_PIN                    (GPIO_PIN_15)
-
 #define W25X_WriteEnable    0x06
 #define W25X_WriteDisable   0x04
 #define W25X_ReadStatusReg  0x05
@@ -52,220 +38,242 @@
 /*  Function Implementations                                                 */
 /*                                                                           */
 /*****************************************************************************/
-void SPI_Send_Byte(U8 out)
+void SPI_Enable(void)
 {
-	U8 i = 0;
-	
-	gpio_bit_reset(CS_GPIO,CS_PIN);
-	for (i = 0; i < 8; i++)
-	{
-		if ((out & 0x80) == 0x80)	                                 //check if MSB is high
-			gpio_bit_set(DI_GPIO, DI_PIN);
-		else
-			gpio_bit_reset(DI_GPIO, DI_PIN);		                                     //if not, set to low
-		__NOP();
-		__NOP();
-		gpio_bit_set(CLK_GPIO, CLK_PIN);			                                     //toggle clock high
-		out = (out << 1);		                                     //shift 1 place for next bit
-		__NOP();
-		__NOP();
-		__NOP();
-	  __NOP();
-		__NOP();
-		gpio_bit_reset(CLK_GPIO, CLK_PIN);			                                     //toggle clock low
-	}
+//#ifdef IO_SIMULATE_SPI
+    gpio_bit_reset(CS_GPIO, CS_PIN);
+//#else
+//    spi_enable(W25Q_SPI);
+//#endif
 }
 
-U8 SPI_Get_Byte(void)
-{	
-	U8 i = 0, in = 0, temp = 0;
-	
-	gpio_bit_reset(CS_GPIO, CS_PIN);
-	for (i = 0; i < 8; i++)
-	{	
-		__NOP();
-		__NOP();
-		in = (in << 1);						                         //shift 1 place to the left or shift in 0
-		temp = gpio_input_bit_get(DO_GPIO, DO_PIN);;						                         //save input
-		gpio_bit_set(CLK_GPIO, CLK_PIN);						                         //toggle clock high
-		__NOP();
-		__NOP();
-		if (temp == 1)						                         //check to see if bit is high
-			in |= 0x01;						                         //if high, make bit high
-		gpio_bit_reset(CLK_GPIO, CLK_PIN);						                         //toggle clock low
-	}	
-	return in;
+void SPI_Disable(void)
+{
+//#ifdef IO_SIMULATE_SPI
+    gpio_bit_set(CS_GPIO, CS_PIN);
+//#else
+//    spi_disable(W25Q_SPI);
+//#endif
 }
 
-U8 SPI_Read_StatusReg(void)
+void SPI_Send_Byte(unsigned char out)
+{
+#ifdef IO_SIMULATE_SPI
+    unsigned char i = 0;
+    for (i = 0; i < 8; i++)
+    {
+        if ((out & 0x80) == 0x80)	                                 //check if MSB is high
+            gpio_bit_set(DI_GPIO, DI_PIN);
+        else
+            gpio_bit_reset(DI_GPIO, DI_PIN);		                                     //if not, set to low
+        __NOP();
+        __NOP();
+        gpio_bit_set(CLK_GPIO, CLK_PIN);			                                     //toggle clock high
+        out = (out << 1);		                                     //shift 1 place for next bit
+        __NOP();
+        __NOP();
+        __NOP();
+        __NOP();
+        __NOP();
+        gpio_bit_reset(CLK_GPIO, CLK_PIN);			                                     //toggle clock low
+    }
+#else
+    spi_i2s_data_transmit(W25Q_SPI,out);
+#endif
+}
+
+unsigned char SPI_Get_Byte(void)
+{
+#ifdef IO_SIMULATE_SPI
+    unsigned char i = 0, in = 0, temp = 0;
+    for (i = 0; i < 8; i++)
+    {
+        __NOP();
+        __NOP();
+        in = (in << 1);						                         //shift 1 place to the left or shift in 0
+        temp = gpio_input_bit_get(DO_GPIO, DO_PIN);;						                         //save input
+        gpio_bit_set(CLK_GPIO, CLK_PIN);						                         //toggle clock high
+        __NOP();
+        __NOP();
+        if (temp == 1)						                         //check to see if bit is high
+            in |= 0x01;						                         //if high, make bit high
+        gpio_bit_reset(CLK_GPIO, CLK_PIN);						                         //toggle clock low
+    }
+    return in;
+#else
+    return (unsigned char)spi_i2s_data_receive(W25Q_SPI);
+#endif
+}
+
+unsigned char SPI_Read_StatusReg(void)
 {	
-	U8 byte = 0;
-	
-	gpio_bit_reset(CS_GPIO, CS_PIN);							                         //enable device
-	SPI_Send_Byte(W25X_ReadStatusReg);		                         //send Read Status Register command
-	byte = SPI_Get_Byte();					                         //receive byte
-	gpio_bit_set(CS_GPIO, CS_PIN);							                         //disable device	
-	return byte;
+    unsigned char byte = 0;
+    SPI_Enable();
+    SPI_Send_Byte(W25X_ReadStatusReg);		                         //send Read Status Register command
+    byte = SPI_Get_Byte();					                         //receive byte
+    SPI_Disable();
+    return byte;
 }
 
 void SPI_Wait_Busy(void)
 {
- 	//waste time until not busy WEL & Busy bit all be 1 (0x03).	
-	//while(SPI_Read_StatusReg()==0x03)
-	while ((SPI_Read_StatusReg()&0x03) == 0x03)
-	{
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-	}
+    //waste time until not busy WEL & Busy bit all be 1 (0x03).
+    //while(SPI_Read_StatusReg()==0x03)
+    while ((SPI_Read_StatusReg()&0x03) == 0x03)
+    {
+        __NOP();
+        __NOP();
+        __NOP();
+        __NOP();
+    }
 }
 
 
 void SPI_Write_Enable(void)
 {	
-	gpio_bit_reset(CS_GPIO, CS_PIN);							                         //enable device
-	SPI_Send_Byte(W25X_WriteEnable);		                         //send W25X_Write_Enable command
-	gpio_bit_set(CS_GPIO, CS_PIN);							                         //disable device
+    SPI_Enable();							                         //enable device
+    SPI_Send_Byte(W25X_WriteEnable);		                         //send W25X_Write_Enable command
+    SPI_Disable();							                         //disable device
 }
 
-void SPI_Read_nBytes(U32 Dst_Addr, U8 nBytes,U8 *header)//num=1,2
+void SPI_Read_nBytes(unsigned int Dst_Addr, unsigned char nBytes,unsigned char *header)//num=1,2
 {
-	U8 i;
+    unsigned char i;
 
-		gpio_bit_reset(CS_GPIO, CS_PIN);										             //enable device
-		SPI_Send_Byte(W25X_ReadData);						             //read command
-		SPI_Send_Byte((U8)((Dst_Addr & 0xFFFFFF) >> 16));		     //send 3 address bytes
-		SPI_Send_Byte((U8)((Dst_Addr & 0xFFFF) >> 8));
-		SPI_Send_Byte((U8)(Dst_Addr & 0xFF));
-		for (i = 0; i < nBytes; i++)			                         //read until no_bytes is reached
-		{
-			*(header+i) = SPI_Get_Byte();					         //receive byte and store at address 80H - FFH
-		}
-		gpio_bit_set(CS_GPIO, CS_PIN);									                 //disable device
-	
+    SPI_Enable();										             //enable device
+    SPI_Send_Byte(W25X_ReadData);						             //read command
+    SPI_Send_Byte((unsigned char)((Dst_Addr & 0xFFFFFF) >> 16));		     //send 3 address bytes
+    SPI_Send_Byte((unsigned char)((Dst_Addr & 0xFFFF) >> 8));
+    SPI_Send_Byte((unsigned char)(Dst_Addr & 0xFF));
+    for (i = 0; i < nBytes; i++)			                         //read until no_bytes is reached
+    {
+        *(header+i) = SPI_Get_Byte();					         //receive byte and store at address 80H - FFH
+    }
+    SPI_Disable();									                 //disable device
+
 }
 
-void SPI_Write_nBytes(U32 Dst_Addr, U8 nBytes, U8 *header)
+void SPI_Write_nBytes(unsigned int Dst_Addr, unsigned char nBytes, unsigned char *header)
 {	
-	U8 i, byte;
+    unsigned char i, byte;
 
 
-		gpio_bit_reset(CS_GPIO, CS_PIN);					                                 //enable device
-		SPI_Write_Enable();				                                 //set WEL
-		gpio_bit_reset(CS_GPIO, CS_PIN);
-		SPI_Send_Byte(W25X_PageProgram); 		                         //send Byte Program command
-		SPI_Send_Byte((U8)((Dst_Addr & 0xFFFFFF) >> 16));	         //send 3 address bytes
-		SPI_Send_Byte((U8)((Dst_Addr & 0xFFFF) >> 8));
-		SPI_Send_Byte((U8)(Dst_Addr & 0xFF));
-		
-		for (i = 0; i < nBytes; i++)
-		{
-			byte = *(header+i);
-			SPI_Send_Byte(byte);		                                 //send byte to be programmed
-		}	
-		gpio_bit_set(CS_GPIO, CS_PIN);			//disable device		
-		
-}
+    SPI_Enable();					                                 //enable device
+    SPI_Write_Enable();				                                 //set WEL
+    SPI_Enable();
+    SPI_Send_Byte(W25X_PageProgram); 		                         //send Byte Program command
+    SPI_Send_Byte((unsigned char)((Dst_Addr & 0xFFFFFF) >> 16));	         //send 3 address bytes
+    SPI_Send_Byte((unsigned char)((Dst_Addr & 0xFFFF) >> 8));
+    SPI_Send_Byte((unsigned char)(Dst_Addr & 0xFF));
 
-void SPI_Erase_Block(U32 Dst_Addr)
-{
-
-		gpio_bit_reset(CS_GPIO, CS_PIN);										             //enable device
-		SPI_Write_Enable();									             //set WEL
-		gpio_bit_reset(CS_GPIO, CS_PIN);
-		SPI_Send_Byte(W25X_64KBErase);			                         //send Sector Erase command
-		SPI_Send_Byte((U8)((Dst_Addr & 0xFFFFFF) >> 16)); 	         //send 3 address bytes
-		SPI_Send_Byte((U8)((Dst_Addr & 0xFFFF) >> 8));
-		SPI_Send_Byte((U8)(Dst_Addr & 0xFF));
-		gpio_bit_set(CS_GPIO, CS_PIN);			//disable device
+    for (i = 0; i < nBytes; i++)
+    {
+        byte = *(header+i);
+        SPI_Send_Byte(byte);		                                 //send byte to be programmed
+    }
+    SPI_Disable();			//disable device
 
 }
 
-void SPI_Erase_Half_Block(U32 Dst_Addr)
+void SPI_Erase_Block(unsigned int Dst_Addr)
 {
-		gpio_bit_reset(CS_GPIO, CS_PIN);										             //enable device
-		SPI_Write_Enable();									             //set WEL
-		gpio_bit_reset(CS_GPIO, CS_PIN);
-		SPI_Send_Byte(W25X_32KBErase);			                         //send Sector Erase command
-		SPI_Send_Byte((U8)((Dst_Addr & 0xFFFFFF) >> 16)); 	         //send 3 address bytes
-		SPI_Send_Byte((U8)((Dst_Addr & 0xFFFF) >> 8));
-		SPI_Send_Byte((U8)(Dst_Addr & 0xFF));
-		gpio_bit_set(CS_GPIO, CS_PIN);					                                 //disable device
+    SPI_Enable();										             //enable device
+    SPI_Write_Enable();									             //set WEL
+    SPI_Enable();
+    SPI_Send_Byte(W25X_64KBErase);			                         //send Sector Erase command
+    SPI_Send_Byte((unsigned char)((Dst_Addr & 0xFFFFFF) >> 16)); 	         //send 3 address bytes
+    SPI_Send_Byte((unsigned char)((Dst_Addr & 0xFFFF) >> 8));
+    SPI_Send_Byte((unsigned char)(Dst_Addr & 0xFF));
+    SPI_Disable();			//disable device
+
 }
 
-void SPI_Erase_Sector(U32 Dst_Addr)
+void SPI_Erase_Half_Block(unsigned int Dst_Addr)
 {
-		gpio_bit_reset(CS_GPIO, CS_PIN);										             //enable device
-		SPI_Write_Enable();									             //set WEL
-		gpio_bit_reset(CS_GPIO, CS_PIN);
-		SPI_Send_Byte(W25X_4KBErase);			                         //send Sector Erase command
-		SPI_Send_Byte((U8)((Dst_Addr & 0xFFFFFF) >> 16)); 	         //send 3 address bytes
-		SPI_Send_Byte((U8)((Dst_Addr & 0xFFFF) >> 8));
-		SPI_Send_Byte((U8)(Dst_Addr & 0xFF));
-		gpio_bit_set(CS_GPIO, CS_PIN);					                                 //disable device
+    SPI_Enable();										             //enable device
+    SPI_Write_Enable();									             //set WEL
+    SPI_Enable();
+    SPI_Send_Byte(W25X_32KBErase);			                         //send Sector Erase command
+    SPI_Send_Byte((unsigned char)((Dst_Addr & 0xFFFFFF) >> 16)); 	         //send 3 address bytes
+    SPI_Send_Byte((unsigned char)((Dst_Addr & 0xFFFF) >> 8));
+    SPI_Send_Byte((unsigned char)(Dst_Addr & 0xFF));
+    SPI_Disable();					                                 //disable device
+}
+
+void SPI_Erase_Sector(unsigned int Dst_Addr)
+{
+    SPI_Enable();										             //enable device
+    SPI_Write_Enable();									             //set WEL
+    SPI_Enable();
+    SPI_Send_Byte(W25X_4KBErase);			                         //send Sector Erase command
+    SPI_Send_Byte((unsigned char)((Dst_Addr & 0xFFFFFF) >> 16)); 	         //send 3 address bytes
+    SPI_Send_Byte((unsigned char)((Dst_Addr & 0xFFFF) >> 8));
+    SPI_Send_Byte((unsigned char)(Dst_Addr & 0xFF));
+    SPI_Disable();					                                 //disable device
 
 }
 
 void SPI_Erase_All(void)
 {
-		gpio_bit_reset(CS_GPIO, CS_PIN);										             //enable device
-		SPI_Write_Enable();									             //set WEL
-		gpio_bit_reset(CS_GPIO, CS_PIN);
-		SPI_Send_Byte(W25X_alKBErase);			                         //send Sector Erase command
-		gpio_bit_set(CS_GPIO, CS_PIN);					                                 //disable device
+    SPI_Enable();										             //enable device
+    SPI_Write_Enable();									             //set WEL
+    SPI_Enable();
+    SPI_Send_Byte(W25X_alKBErase);			                         //send Sector Erase command
+    SPI_Disable();					                                 //disable device
 }
 
-void SPI_erase(U32 address,U16 numbers,U8 mode)
+void SPI_erase(unsigned int address,unsigned short numbers,unsigned char mode)
 {
-	U8 i;
+    unsigned char i;
 
-	if(mode==1)
-	{
-		SPI_Erase_All();
-		return;
-	}
-	if(mode==4)
-	{
-		for(i=0;i<numbers;i++)
-		{
-		 	SPI_Wait_Busy();                                         //0x010000 32KB  U16
-			SPI_Erase_Sector(address);
-			address=address+0x1000;                                  //0x001000 4 KB  U16
-		}
-		return;
-	}
-	if(mode==32)
-	{
-		for(i=0;i<numbers;i++)
-		{
-			SPI_Wait_Busy();
-			SPI_Erase_Half_Block(address);
-			address=address+0x8000;                                  //0x010000 32KB  U16                                   //0x001000 4 KB  U16
-		}
-		return;
-	}
-	if(mode==64)
-	{
-		for(i=0;i<numbers;i++)
-		{
-			SPI_Wait_Busy();
-			SPI_Erase_Block(address);
-			address=address+0x10000;                                 //0x010000 64KB  U16                                   //0x001000 4 KB  U16
-		}
-		return;
-	}
+    if(mode==1)
+    {
+        SPI_Erase_All();
+        return;
+    }
+    if(mode==4)
+    {
+        for(i=0;i<numbers;i++)
+        {
+            SPI_Wait_Busy();                                         //0x010000 32KB  U16
+            SPI_Erase_Sector(address);
+            address=address+0x1000;                                  //0x001000 4 KB  U16
+        }
+        return;
+    }
+    if(mode==32)
+    {
+        for(i=0;i<numbers;i++)
+        {
+            SPI_Wait_Busy();
+            SPI_Erase_Half_Block(address);
+            address=address+0x8000;                                  //0x010000 32KB  U16                                   //0x001000 4 KB  U16
+        }
+        return;
+    }
+    if(mode==64)
+    {
+        for(i=0;i<numbers;i++)
+        {
+            SPI_Wait_Busy();
+            SPI_Erase_Block(address);
+            address=address+0x10000;                                 //0x010000 64KB  U16                                   //0x001000 4 KB  U16
+        }
+        return;
+    }
 }
 
 void SPI_WriteW25X_Disable(void)
 {
-	gpio_bit_reset(CS_GPIO, CS_PIN);							                         //enable device
-	SPI_Send_Byte(W25X_WriteDisable);		                         //send W25X_WriteW25X_DIsable command
-	gpio_bit_set(CS_GPIO, CS_PIN);							                         //disable device
+    SPI_Enable();							                         //enable device
+    SPI_Send_Byte(W25X_WriteDisable);		                         //send W25X_WriteW25X_DIsable command
+    SPI_Disable();							                         //disable device
 }
 
 void SPI_init(void)
 {
+#ifdef IO_SIMULATE_SPI
+
     rcu_periph_clock_enable(W25_RCC1);
     rcu_periph_clock_enable(W25_RCC2);
     rcu_periph_clock_enable(RCU_AF);
@@ -279,7 +287,37 @@ void SPI_init(void)
 
     gpio_bit_reset(CLK_GPIO, CLK_PIN);							                         //set clock to low initial state for SPI operation mode 0
     //	gpio_bit_set(CLK_GPIO, CLK_PIN);							                         //set clock to High initial state for SPI operation mode 3
-    gpio_bit_set(CS_GPIO, CS_PIN);
+#else
+    spi_parameter_struct spi_struct;
+    spi_struct.device_mode = SPI_MASTER;                                                       /*!< SPI master or slave */
+    spi_struct.trans_mode = SPI_TRANSMODE_FULLDUPLEX;                                                        /*!< SPI transfer type */
+    spi_struct.frame_size = SPI_FRAMESIZE_8BIT;                                                        /*!< SPI frame size */
+    spi_struct.nss=SPI_NSS_SOFT;                                                               /*!< SPI NSS control by hardware or software */
+    spi_struct.endian = SPI_ENDIAN_MSB;                                                            /*!< SPI big endian or little endian */
+    spi_struct.clock_polarity_phase=SPI_CK_PL_HIGH_PH_2EDGE;                                              /*!< SPI clock phase and polarity */
+    spi_struct.prescale = SPI_PSC_256;                                                          /*!< SPI prescaler factor */
+    
+    rcu_periph_clock_enable(RCU_AF);
+    rcu_periph_clock_enable(RCU_GPIOA);
+    rcu_periph_clock_enable(RCU_GPIOB);
+    rcu_periph_clock_enable(RCU_SPI2);
+   
+    gpio_pin_remap_config(GPIO_SWJ_NONJTRST_REMAP,ENABLE);
+    gpio_pin_remap_config(GPIO_SWJ_SWDPENABLE_REMAP,ENABLE) ;
+    
+    
+    gpio_init(CS_GPIO, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, CS_PIN);
+    SPI_Disable();
+    
+    gpio_init(CLK_GPIO, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, CLK_PIN);
+    gpio_init(DI_GPIO, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, DI_PIN);
+    gpio_init(DO_GPIO, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, DO_PIN);
+    
+    spi_init(W25Q_SPI, &spi_struct);
+    spi_crc_off(W25Q_SPI);
+#endif
+    
+    SPI_Disable();
     SPI_WriteW25X_Disable();
 
 }
@@ -287,43 +325,26 @@ void SPI_init(void)
 #include <finsh.h>
 void init(void)
 {
-	SPI_init();
+    SPI_init();
 }
 void test(int i)
 {
-	if(i == 0)
-	{
-		unsigned char buffer[8] = {0x00};
-		SPI_Read_nBytes(0x000000 ,8,buffer);
-		printf("Read:%02x %02x %02x %02x %02x %02x %02x %02x\n",
-                buffer[0],buffer[1],buffer[2],buffer[3],buffer[4],buffer[5],buffer[6],buffer[7]);
-	}else if(i == 1)
-	{
-		unsigned char buffer[8]= {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07};
-		SPI_Write_nBytes(0x000000,8,buffer);
-		printf("Write\n");
-	}else if(i == 2)
-	{
-		SPI_Erase_Sector(0x000000);
-		printf("Erase\n");
-	}else if(i == 3)
-	{
-		printf("RESET\n");
-		gpio_bit_reset(CLK_GPIO, CLK_PIN);
-		gpio_bit_reset(DI_GPIO, DI_PIN);
-		gpio_bit_reset(CS_GPIO, CS_PIN);
-	}else if(i == 4)
-	{
-		printf("SET\n");
-		gpio_bit_set(CLK_GPIO, CLK_PIN);
-		gpio_bit_set(DI_GPIO, DI_PIN);
-		gpio_bit_set(CS_GPIO, CS_PIN);
-	}else if(i == 5)
-	{
-		printf("%d %d %d %d\n",gpio_input_bit_get(DO_GPIO, DO_PIN),
-                gpio_input_bit_get(CLK_GPIO, CLK_PIN),gpio_input_bit_get(DI_GPIO, DI_PIN),
-                gpio_input_bit_get(CS_GPIO, CS_PIN));
-	}
+    if(i == 0)
+    {
+        unsigned char buffer[8] = {0x00};
+        SPI_Read_nBytes(0x000000 ,8,buffer);
+        printf("Read:%02x %02x %02x %02x %02x %02x %02x %02x\n",
+               buffer[0],buffer[1],buffer[2],buffer[3],buffer[4],buffer[5],buffer[6],buffer[7]);
+    }else if(i == 1)
+    {
+        unsigned char buffer[8]= {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07};
+        SPI_Write_nBytes(0x000000,8,buffer);
+        printf("Write\n");
+    }else if(i == 2)
+    {
+        SPI_Erase_Sector(0x000000);
+        printf("Erase\n");
+    }
 }
 FINSH_FUNCTION_EXPORT(test, test);
 FINSH_FUNCTION_EXPORT(init, init);
--- driver/W25Q64Flash.h
+++ driver/W25Q64Flash.h
@@ -14,26 +14,44 @@
 /*  Definitions                                                              */
 /*                                                                           */
 /*****************************************************************************/
-typedef unsigned char U8;
-typedef unsigned short U16;
-typedef unsigned int U32;
+#define IO_SIMULATE_SPI //使用IO模拟SPI
 
+
+#define W25_RCC1                    RCU_GPIOA
+#define W25_RCC2                    RCU_GPIOB
+#define CLK_GPIO                   GPIOB
+#define CLK_PIN                    (GPIO_PIN_3)
+
+#define DO_GPIO                   GPIOB
+#define DO_PIN                    (GPIO_PIN_4)
+
+#define DI_GPIO                   GPIOB
+#define DI_PIN                    (GPIO_PIN_5)
+
+#define CS_GPIO                   GPIOA
+#define CS_PIN                    (GPIO_PIN_15)
+#ifdef IO_SIMULATE_SPI
+#else
+#define W25Q_SPI    SPI2
+#endif
 /*****************************************************************************/
 /*                                                                           */
 /*  Function Declarations                                                    */
 /*                                                                           */
 /*****************************************************************************/
-void SPI_Send_Byte(U8 out);
-U8 SPI_Get_Byte(void);
+#ifdef IO_SIMULATE_SPI
+void SPI_Send_Byte(unsigned char out);
+unsigned char SPI_Get_Byte(void);
+#endif
 void SPI_Wait_Busy(void);
 void SPI_Write_Enable(void);		//写使能
-void SPI_Read_nBytes(U32 Dst_Addr, U8 nBytes,U8 *header);//从flash芯片读取数据
-void SPI_Write_nBytes(U32 Dst_Addr, U8 nBytes, U8 *header);//向flash芯片写数据
-void SPI_Erase_Block(U32 Dst_Addr);	//擦除块
-void SPI_Erase_Half_Block(U32 Dst_Addr);	//擦除半块
-void SPI_Erase_Sector(U32 Dst_Addr);		//擦除扇区
+void SPI_Read_nBytes(unsigned int Dst_Addr, unsigned char nBytes,unsigned char *header);//从flash芯片读取数据
+void SPI_Write_nBytes(unsigned int Dst_Addr, unsigned char nBytes, unsigned char *header);//向flash芯片写数据
+void SPI_Erase_Block(unsigned int Dst_Addr);	//擦除块
+void SPI_Erase_Half_Block(unsigned int Dst_Addr);	//擦除半块
+void SPI_Erase_Sector(unsigned int Dst_Addr);		//擦除扇区
 void SPI_Erase_All(void);			//擦除整个芯片
-void SPI_erase(U32 address,U16 numbers,U8 mode);
+void SPI_erase(unsigned int address,unsigned short numbers,unsigned char mode);
 void SPI_WriteW25X_Disable(void);
 void SPI_init(void);
 #endif /*__W25Q64FLASH_H__*/
